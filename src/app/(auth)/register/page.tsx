8eea7b6656abdecccbeb176f8b5561a77d1d17cd
xtrphy <perepeckinvlad@gmail.com>
2025-07-27 14:17:18 +0600

    Redirect to dashboard user if logged in

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,10 +1,26 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect } from 'react';
 import { BreadcrumbWithDropdown } from '@/components/Breadcrumb';
 import RegisterForm from '@/components/RegisterForm';
+import { useRouter } from 'next/navigation';
+import { supabase } from '@/utils/supabaseClient';
 
 const RegisterPage = () => {
+    const router = useRouter();
+
+    useEffect(() => {
+        const checkAuth = async () => {
+            const { data: { user } } = await supabase.auth.getUser();
+
+            if (user) {
+                router.replace('/dashboard');
+            }
+        }
+
+        checkAuth();
+    }, [router]);
+
     return (
         <div className='flex flex-col gap-3'>
             <BreadcrumbWithDropdown />
